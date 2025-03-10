dfa1a785c6c46bf4cf5a73349668a56dc9b68161
GitHub Action <action@github.com>
2025-03-10 01:26:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/prvaca/moviageneradorpreview/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/prvaca/moviageneradorpreview/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.prvaca.moviageneradorpreview.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.prvaca.moviageneradorpreview.MyBundle
+import com.github.prvaca.moviageneradorpreview.services.MyProjectService
 import javax.swing.JButton
 
 
